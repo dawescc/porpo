4f46c163acc32702ea1b7a60e25b9b9223093ddd
dtech-auto <105608038+dtech-auto@users.noreply.github.com>
2022-05-16 17:02:07 -0400

    Fixed Issues #1 and #2

--- gui.py
+++ gui.py
@@ -3,11 +3,13 @@
 
 """
 # Imports
+from turtle import right
 import PySimpleGUI as sg
 import fastf1
 from fastf1 import plotting
 import matplotlib.pylab as plt
 import os
+import datetime
 
 # Declare Cache and Export Path
 cache_path = '~/Documents/F1 Data Analysis/Cache/'
@@ -30,9 +32,13 @@ fastf1.Cache.enable_cache(cache_path)
 # Begin Program
 ####
 
+# Create Year Range for Year Picker
+cur_year = datetime.datetime.today().year
+year_list = range(2018,cur_year+1)
+
 sg.theme('DarkRed')
 main_layout = [
-    [sg.Text('Grand Prix Year:'), sg.Push(), sg.InputText(size=15, justification='right')],
+    [sg.OptionMenu(year_list, default_value=f'{cur_year}', expand_x=True)],
     [sg.Button('Load', expand_x=True, bind_return_key=True)]
 ]
 
@@ -63,8 +69,9 @@ while True:
                     grand_prix = gp_values[0]
 
                     ses_list = ['FP1', 'FP2', 'FP3', 'S', 'Q', 'R']
-                    ses_win_layout = [[sg.OptionMenu(ses_list, default_value=ses_list[0], size=15),
-                                       sg.Button('Load', expand_x=True, bind_return_key=True)]]
+                    ses_win_layout = [[sg.Text(f'{grand_prix} Sessions:')],
+                                        [sg.OptionMenu(ses_list, default_value=ses_list[0], size=15)],
+                                        [sg.Button('Load', expand_x=True, bind_return_key=True)]]
                     ses_window = sg.Window('Session Selection', ses_win_layout, keep_on_top=True)
 
                     window.close()
@@ -83,8 +90,8 @@ while True:
             session = fastf1.get_session(InputVars.year, SeasonSchedule.grand_prix, SeasonSchedule.ses_type)
 
             class ProgBar:
-                prog_win_layout = [[sg.ProgressBar(30, orientation='h', size=(20, 20), key='PROGRESSBAR')]]
-                prog_win = sg.Window('Loading Drivers', prog_win_layout, auto_close=True, finalize=True)
+                prog_win_layout = [[sg.Text('Loading Drivers and Session Data...')], [sg.ProgressBar(30, orientation='h', size=(20, 20), key='PROGRESSBAR',)]]
+                prog_win = sg.Window('Loading Data', prog_win_layout, auto_close=True, finalize=True)
                 progress_bar = prog_win['PROGRESSBAR']
                 for i in range(15):
                     progress_bar.update(current_count=i + 1)
@@ -106,7 +113,7 @@ while True:
                 [sg.Text('Select Driver:'), sg.OptionMenu(driver_list, default_value=driver_list[0], size=15)],
                 [sg.Button('Load', bind_return_key=True, expand_x=True)]
             ]
-            driver_window = sg.Window('Grand Prix Selection', driver_win_layout, keep_on_top=True)
+            driver_window = sg.Window('Driver Selection', driver_win_layout, keep_on_top=True)
             while True:
                 driver_event, driver_values = driver_window.read()
                 if driver_event == sg.WIN_CLOSED:
@@ -123,11 +130,11 @@ while True:
             team = info['TeamName']
             team_color = fastf1.plotting.team_color(team)
 
-            lap_or_ses_list = ['Lap', 'Full Session', 'Fastest']
-            lap_or_ses_win_layout = [[sg.OptionMenu(lap_or_ses_list, default_value=lap_or_ses_list[0], size=15),
+            lap_or_ses_list = ['Lap', 'Full Session', 'Fastest Lap']
+            lap_or_ses_win_layout = [[sg.Text('Evaluate the full session, or a lap?')], [sg.OptionMenu(lap_or_ses_list, default_value=lap_or_ses_list[0], size=15),
                                       sg.Button('Load', expand_x=True, bind_return_key=True)]]
 
-            lap_or_ses_window = sg.Window('Session Selection', lap_or_ses_win_layout, keep_on_top=True)
+            lap_or_ses_window = sg.Window('Lap Selection', lap_or_ses_win_layout, keep_on_top=True)
             while True:
                 lap_or_ses_event, lap_or_ses_values = lap_or_ses_window.read()
                 if lap_or_ses_event == sg.WIN_CLOSED:
@@ -136,38 +143,42 @@ while True:
                     lap_or_ses = f'{lap_or_ses_values[0]}'
                     if lap_or_ses == 'Lap':
                         lap_slider_layout = [[sg.Slider((ses['LapNumber'].min(), ses['LapNumber'].max()), orientation='h')], 
-                                            [sg.Button('Load', bind_return_key=True, expand_x=True)]
+                                            [sg.Button('Select', bind_return_key=True, expand_x=True)]
                                             ]
                         lap_slider_win = sg.Window('Select Lap', lap_slider_layout, keep_on_top=True)
                         lap_slider_event, num = lap_slider_win.read()
                         if lap_slider_event == sg.WIN_CLOSED:
                             break
-                        if lap_slider_event == 'Load' or 'Return':
+                        if lap_slider_event == 'Select' or 'Return':
                             pass
                         lap_n = ses[ses['LapNumber'] == num[0]]
                         lap_n_tel = lap_n.get_telemetry()
                         data = lap_n_tel
                         title = f'{fullname} {SessionInfo.event_name} Lap {num[0]} Data:'
+                        def_xvalue = f'Distance'
                         lap_slider_win.close()
 
-                    elif lap_or_ses == 'Fastest':
+                    elif lap_or_ses == 'Fastest Lap':
                         f_lap = ses.pick_fastest()
                         fastest_lap = f_lap.get_telemetry()
                         data = fastest_lap
                         title = f'{fullname} {SessionInfo.event_name} Fastest Lap Data:'
+                        def_xvalue = f'Distance'
 
                     else:
                         data = ses
                         title = f'{fullname} {SessionInfo.event_name} Full {SeasonSchedule.ses_type} Session Data:'
+                        def_xvalue = f'LapNumber'
 
                     lap_or_ses_window.close()
 
         var_list = list(DriverInfo.data)
         var_win_layout = [
             [sg.Text(f'{DriverInfo.title}')],
-            [sg.Text('Y Variable:'), sg.OptionMenu(var_list, default_value=var_list[0])],
-            [sg.Text('X Variable:'), sg.OptionMenu(var_list, default_value=var_list[12])],
-            [sg.Button('Plot')]]
+            [sg.Text('Y Variable:'), sg.OptionMenu(var_list, default_value=var_list[0], expand_x=True)],
+            [sg.Text('X Variable:'), sg.OptionMenu(var_list, default_value=DriverInfo.def_xvalue, expand_x=True)],
+            [sg.Text('*Note it is not recomended to change the X Variable from "Distance" or "LapNumber"')],
+            [sg.Button('Plot', bind_return_key=True, expand_x=True)]]
 
         var_window = sg.Window('Variable Selection', var_win_layout, keep_on_top=True)
         while True:
