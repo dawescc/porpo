4f07fdc9d53108854f85a91ab26263ca0cc91f82
dtech-auto <105608038+dtech-auto@users.noreply.github.com>
2022-05-17 13:04:05 -0400

    update readme

--- scripts/gui.py
+++ scripts/gui.py
@@ -197,7 +197,7 @@ while True:
                 xmin, xmax = x.min(), x.max()
 
                 #plt.style.use('dark_background')
-                fig = plt.figure(1)
+                fig = plt.figure(1, figsize=(16,9))
                 plot1 = fig.subplots()
                 plot1.plot(x, y, color=DriverInfo.team_color, label=f"{y.name}")
                 plot1.set_ylabel(f"{y.name}")
